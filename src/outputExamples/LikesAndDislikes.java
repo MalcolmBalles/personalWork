46f8520a2ee60b1b99c8d0d0543893cb8f736b98
Malcolm Balles <19ballem@elmbrookstudents.org>
2018-09-13 13:43:38 -0500

    Likes and Dislikes code opurntiysdiuty

---
+++ src/outputExamples/LikesAndDislikes.java
@@ -0,0 +1,15 @@
+package outputExamples;
+
+public class LikesAndDislikes {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		System.out.println("Likes\t\t\t\tDislikes\n");
+		System.out.println("Food\t\t\t\tReading");
+		System.out.println("Sleep\t\t\t\tWritting");
+		System.out.println("Netflix\t\t\t\tCulvers");
+		System.out.println("Ishaan\t\t\t\tSchool");
+		System.out.println("TI-84\t\t\t\tTyping out this code");
+	}
+
+}
