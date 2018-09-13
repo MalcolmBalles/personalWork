1914e5d7a14b0935a05ff23341bca6626a2b155f
Malcolm Balles <19ballem@elmbrookstudents.org>
2018-09-13 13:32:37 -0500

    added vote thing, deleted something possibly?

---
+++ src/outputExamples/YearTillMeVote.java
@@ -0,0 +1,9 @@
+package outputExamples;
+
+public class YearTillMeVote {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		System.out.println(".041 Year until I can vote"); //display year until i can vote
+	}
+}
