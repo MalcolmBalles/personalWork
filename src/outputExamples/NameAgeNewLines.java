831c011d9f18c141b9daebfb15470266b9f7d293
Malcolm Balles <19ballem@elmbrookstudents.org>
2018-09-13 13:17:14 -0500

    First last name WITH age all different lines (tricky but i did it)

---
+++ src/outputExamples/NameAgeNewLines.java
@@ -0,0 +1,12 @@
+package outputExamples;
+
+public class NameAgeNewLines {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		System.out.println("Malcolm"); //display first name on first line
+		System.out.println("Balles"); //display last name on second line
+		System.out.println("17"); //display age on third line
+	}
+
+}
