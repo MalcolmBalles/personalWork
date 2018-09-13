970b05f53f19b82e4ee6cb2d93af01bc65b2afb1
Malcolm Balles <19ballem@elmbrookstudents.org>
2018-09-13 13:10:37 -0500

    Malcolm Balles but 2 lines this time ;)

---
+++ src/outputExamples/MalcolmBalles2Lines.java
@@ -0,0 +1,11 @@
+package outputExamples;
+
+public class MalcolmBalles2Lines {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		System.out.println("Malcolm"); //display first name on first line
+		System.out.println("Balles"); //display last name on second line
+	}
+
+}
