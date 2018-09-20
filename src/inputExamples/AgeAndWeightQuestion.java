33af86e0312abb9b6e9e19767b801a9e57fc8312
Malcolm Balles <19ballem@elmbrookstudents.org>
2018-09-20 13:25:36 -0500

    age and wieght program

---
+++ src/inputExamples/AgeAndWeightQuestion.java
@@ -0,0 +1,17 @@
+package inputExamples;
+
+import java.util.Scanner;
+
+public class AgeAndWeightQuestion {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+
+		System.out.println("What is your age (in years)?");
+		Scanner input = new Scanner(System.in);
+		int age = input.nextInt();
+		System.out.println("What is your wieght (in pounds)?");
+		int weight = input.nextInt();
+		System.out.println("You are " + age + " years old and you weigh " + weight + " pounds");
+	}
+}
