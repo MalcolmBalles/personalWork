ebd6ec50310aac5948ae994f4b960fe10cab1364
Malcolm Balles <19ballem@elmbrookstudents.org>
2018-10-18 12:49:55 -0500

    things that are def related to this unit...

---
+++ src/decisionStructures/ForbiddenClass.java
@@ -0,0 +1,16 @@
+package decisionStructures;
+
+public class ForbiddenClass {
+
+	public ForbiddenClass() {
+
+		System.out.println("                                                           ");
+		System.out.println("                                   \\   \\  \\               ");
+		System.out.println("                                    |   |  |               ");
+		System.out.println("                                    |   |  |               ");
+		System.out.println("                            ____    |  /  /                ");
+		System.out.println("                           |     \\  | / /                  ");
+		System.out.println("                            \\_____\\ |/ /                   ");
+		System.out.println("                                  \\\\||/                    ");
+	}
+}
---
+++ src/decisionStructures/Test.java
@@ -0,0 +1,10 @@
+package decisionStructures;
+
+public class Test {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		ForbiddenClass dont = new ForbiddenClass();
+	}
+
+}
