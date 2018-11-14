6d189fc03e1172e60c4c63759521faca5826a33c
Malcolm Balles <19ballem@elmbrookstudents.org>
2018-11-14 14:04:58 -0600

    just another commit

--- src/strings/ClassNotes1.java
+++ src/strings/ClassNotes1.java
@@ -1,10 +1,16 @@
 package strings;
-
+import java.util.Scanner;
 public class ClassNotes1 {
 
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
 
+		Scanner input = new Scanner(System.in);
+		System.out.println("say a sentence");
+		String name = input.nextLine();
+		boolean ritu = name.contains("ritu");
+		System.out.println(ritu);
+		System.out.println(name.indexOf("ur mom gay"));
 	}
 
 }
